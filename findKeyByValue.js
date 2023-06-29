5f577ad8b7b3142c65eba1a145ae9bd6b4b7ff02
Benson <bensonwlee@gmail.com>
2023-06-29 06:02:33 -0400

    Implement findKeyByValue in Lotide

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// Function Calculates Primitive Values
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😊 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Function finds key that has argument value
+const findKeyByValue = function (object, shows) {
+  const keys = Object.keys(object);
+  // Search all keys for shows
+  for (const element of keys) {
+    if (object[element] === shows) {
+      return element;
+    }
+  }
+}
+
+// Test
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
