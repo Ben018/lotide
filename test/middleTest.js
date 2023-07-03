47aab1c84796ef496cd6e948677377eb05eedff2
Benson <bensonwlee@gmail.com>
2023-07-03 14:51:05 -0400

    added mocha test for middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,24 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
+const { it } = require("mocha");
 const middle = require("../middle");
 
-// Test
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+describe("#middle", () => {
+  it("return [] if 1 element is entered", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("return [] if 2 elements are entered", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("return 2 if [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("return 3 if [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("return [2, 3] if [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("return [3, 4] if [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
